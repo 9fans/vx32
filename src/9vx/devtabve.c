c72804dc9efe5d77c2ffae3a447e06e89836d6c5
Jesus Galan Lopez (yiyus) <yiyu.jgl@gmail.com>
2010-06-03 20:45:26 +0200

    both PLAN9NET options compile

--- src/9vx/devtabve.c
+++ src/9vx/devtabve.c
@@ -7,6 +7,7 @@
 #include "ip/ip.h"
 #include "sd.h"
 
+extern Dev aoedevtab;
 extern Dev consdevtab;
 extern Dev rootdevtab;
 extern Dev pipedevtab;
@@ -26,15 +27,21 @@ extern Dev mntloopdevtab;
 extern Dev dupdevtab;
 extern Dev sddevtab;
 extern Dev capdevtab;
+extern Dev etherdevtab;
+
+extern SDifc sdloopifc;
+extern SDifc sdaoeifc;
 
 Dev *devtab[] = {
 	&rootdevtab,	/* must be first */
+	&aoedevtab,
 	&audiodevtab,
 	&capdevtab,
 	&consdevtab,
 	&drawdevtab,
 	&dupdevtab,
 	&envdevtab,
+	&etherdevtab,
 	&fsdevtab,
 	&ipdevtab,
 	&mntdevtab,
@@ -50,12 +57,42 @@ Dev *devtab[] = {
 	0
 };
 
-void
-links(void)
-{
+extern void ethervelink(void);
+extern void ethermediumlink(void);
+extern void loopbackmediumlink(void);
+extern void netdevmediumlink(void);
+void links(void) {
+	ethermediumlink();
+	loopbackmediumlink();
+	netdevmediumlink();
+	ethervelink();
 }
+
+extern void ilinit(Fs*);
+extern void tcpinit(Fs*);
+extern void udpinit(Fs*);
+extern void ipifcinit(Fs*);
+extern void icmpinit(Fs*);
+extern void icmp6init(Fs*);
+extern void greinit(Fs*);
+extern void ipmuxinit(Fs*);
+extern void espinit(Fs*);
+void (*ipprotoinit[])(Fs*) = {
+	ilinit,
+	tcpinit,
+	udpinit,
+	ipifcinit,
+	icmpinit,
+	icmp6init,
+	greinit,
+	ipmuxinit,
+	espinit,
+	nil,
+};
 
 SDifc *sdifc[] =
 {
+	&sdloopifc,
+	&sdaoeifc,
 	0,
 };

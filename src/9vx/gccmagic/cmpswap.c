78076513d0f292cd4b20fbe69c83703ffec048e9
rminnich@gmail.com <rminnich@gmail.com>
2011-01-24 09:04:20 -0800

    clean up and force 32 bit compiles

--- src/9vx/gccmagic/cmpswap.c
+++ src/9vx/gccmagic/cmpswap.c
@@ -1,10 +1,7 @@
 int	oscmpswap(long* addr, long oldval, long newval)
 {
-	bool res;
-	print("oscmpswap: addr %p, *addr %ld, oldval %ld, newval %ld\n", 
-			addr, *addr, oldval, newval);
+	int res;
 	res = __sync_bool_compare_and_swap(addr, oldval, newval);
-	print("oscmpswap: result %d\n", res);
 	if (res) 
 		return 1;
 	return 0;

611cf156b8b3fb30866dad8dcee8053da09f704c
rminnich@gmail.com <rminnich@gmail.com>
2011-01-24 08:59:49 -0800

    everything is 64bit

--- src/9vx/gccmagic/cmpswap.c
+++ src/9vx/gccmagic/cmpswap.c
@@ -1,5 +1,12 @@
 int	oscmpswap(long* addr, long oldval, long newval)
 {
-	return __sync_bool_compare_and_swap(addr, oldval, newval);
+	bool res;
+	print("oscmpswap: addr %p, *addr %ld, oldval %ld, newval %ld\n", 
+			addr, *addr, oldval, newval);
+	res = __sync_bool_compare_and_swap(addr, oldval, newval);
+	print("oscmpswap: result %d\n", res);
+	if (res) 
+		return 1;
+	return 0;
 }
 
